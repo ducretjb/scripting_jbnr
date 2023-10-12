df50508bb7b4407a0c39c30775ec61cfecd287e4
Natalia <natalia.sviattseva@gmail.com>
2023-10-12 14:54:30 +0200

    Reverting to the state of the project at 8388817

---
+++ radis_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Mise à jour des paquets existants
+sudo apt-get update
+
+# Installation de Redis
+sudo apt-get install redis-server
+
+# Démarrage du service Redis
+sudo systemctl start redis-server
+
+# Activer Redis pour qu'il démarre automatiquement au démarrage
+sudo systemctl enable redis-server
+
+# Afficher le statut du service Redis
+sudo systemctl status redis-server
